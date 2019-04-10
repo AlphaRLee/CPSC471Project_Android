29b5215b2c81326adf6de7d622dda312e0717b85
erik <erik.skoronski@gmail.com>
2019-04-10 16:18:21 -0600

    added missing files

---
+++ app/src/main/java/com/example/cpsc471expensesubmitter/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.cpsc471expensesubmitter;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private TextView mTextMessage;
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            Fragment selectedFragment = null;
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+//                case R.id.navigation_newexpense:
+//                    mTextMessage.setText(R.string.title_newexpense);
+//                    return true;
+//                case R.id.navigation_account:
+//                    mTextMessage.setText(R.string.title_account);
+//                    return true;
+//            }
+//
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    selectedFragment).commit();
+//
+//            return false;
+//        }
+//    };
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Button functionality
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            //@Nullable
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        // End of button functionality
+
+    }
+
+    // Puts image taken by user onto screen as preview
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
+
+}
